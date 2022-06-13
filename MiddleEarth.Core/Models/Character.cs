5b6e66161e8565d9f44ac0c038a0d355b9e29559
gokcaneren <gokcann01@gmail.com>
2022-06-13 17:25:37 +0300

    Entities are created!

---
+++ MiddleEarth.Core/Models/Character.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Core.Models
+{
+    public class Character:BaseEntity
+    {
+        public string Name { get; set; }
+
+        public int RaceId { get; set; }
+        public Race Race { get; set; }
+
+        public int RealmsId { get; set; }
+        public Realms Realms { get; set; }
+
+        public int CharacterClassId { get; set; }
+        public CharacterClass CharacterClass { get; set; }
+
+        public int WeaponId { get; set; }
+        public Weapon Weapon { get; set; }
+
+        public CharacterFeature CharacterFeature { get; set; }
+    }
+}
---
+++ MiddleEarth.Core/Models/CharacterFeature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Core.Models
+{
+    public class CharacterFeature
+    {
+        public int Id { get; set; }
+        public string Sex { get; set; }
+        public int Height { get; set; }
+        public string Eyes { get; set; }
+
+        public int CharacterId { get; set; }
+        public Character Character { get; set; }
+    }
+}
