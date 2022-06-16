38cbdb4b7c7415d2f937801cea18659955006d46
gokcaneren <gokcann01@gmail.com>
2022-06-16 15:05:44 +0300

    IUnitOfWork interface added!

---
+++ MiddleEarth.Core/UnitOfWorks/IUnitOfWokr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Core.UnitOfWorks
+{
+    public interface IUnitOfWokr
+    {
+        Task CommitAsync();
+        void Commit();
+    }
+}
