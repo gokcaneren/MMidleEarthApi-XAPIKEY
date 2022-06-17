0b45a8b27e986fde18e25e4ec89eade74529c351
gokcaneren <gokcann01@gmail.com>
2022-06-17 15:41:10 +0300

    Configurations completed!

---
+++ MiddleEarth.Repository/Configurations/CharacterFeatureConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MiddleEarth.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Repository.Configurations
+{
+    internal class CharacterFeatureConfiguration : IEntityTypeConfiguration<CharacterFeature>
+    {
+        public void Configure(EntityTypeBuilder<CharacterFeature> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x=>x.Id).UseIdentityColumn();
+
+            builder.HasOne(c => c.Character).WithOne(c => c.CharacterFeature)
+                .HasForeignKey<CharacterFeature>(c => c.CharacterId);
+        }
+    }
+}
