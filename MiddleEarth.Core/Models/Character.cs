fbfcc3d52a8cc85f22d4d360b051704ecfbab9a6
gokcaneren <gokcann01@gmail.com>
2022-06-21 15:25:54 +0300

    CharacterSeed completed!

--- MiddleEarth.Core/Models/Character.cs
+++ MiddleEarth.Core/Models/Character.cs
@@ -9,16 +9,14 @@ namespace MiddleEarth.Core.Models
     public class Character:BaseEntity
     {
         public string Name { get; set; }
-
+        public string ImageUrl { get; set; }
         public int RaceId { get; set; }
         public Race Race { get; set; }
 
-        public int RealmId { get; set; }
-        public Realm Realm { get; set; }
+        public int? RealmId { get; set; }
+        public Realm? Realm { get; set; }
 
         public ICollection<Weapon> Weapons { get; set; }
-
-        public string ImageUrl { get; set; }
 
         public CharacterFeature CharacterFeature { get; set; }
     }
---
+++ MiddleEarth.Repository/Seeds/CharacterSeed.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MiddleEarth.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Repository.Seeds
+{
+    internal class CharacterSeed : IEntityTypeConfiguration<Character>
+    {
+        public void Configure(EntityTypeBuilder<Character> builder)
+        {
+            builder.HasData(
+                new Character
+                {
+                    Id = 1,
+                    Name = "Gandalf",
+                    ImageUrl = "https://static.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest/scale-to-width-down/350?cb=20121110131754",
+                    RaceId = 7,
+                    RealmId = null,
+                },
+                new Character
+                {
+                    Id = 2,
+                    Name = "Melkor",
+                    ImageUrl = "https://static.wikia.nocookie.net/lotr/images/8/82/Melkor_by_formenost.jpg/revision/latest/scale-to-width-down/350?cb=20121205131822",
+                    RaceId = 8,
+                    RealmId = null,
+                }
+                );
+        }
+    }
+}
