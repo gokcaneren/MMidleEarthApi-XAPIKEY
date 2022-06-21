6a70e6721d6361524133f15b0f00d4c104f39cf0
gokcaneren <gokcann01@gmail.com>
2022-06-21 15:59:43 +0300

    UnitOfWorks completed!

--- MiddleEarth.Core/UnitOfWorks/IUnitOfWork.cs
+++ MiddleEarth.Core/UnitOfWorks/IUnitOfWork.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MiddleEarth.Core.UnitOfWorks
 {
-    public interface IUnitOfWokr
+    public interface IUnitOfWork
     {
         Task CommitAsync();
         void Commit();
