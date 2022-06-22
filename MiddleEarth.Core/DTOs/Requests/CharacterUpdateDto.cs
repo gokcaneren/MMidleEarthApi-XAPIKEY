5283aa41b86ff355337b156fe810eda024cbdba4
gokcaneren <gokcann01@gmail.com>
2022-06-22 14:55:13 +0300

    CharacterUpdateDto added!

---
+++ MiddleEarth.Core/DTOs/Requests/CharacterUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Core.DTOs.Requests
+{
+    public class CharacterUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public int RaceId { get; set; }
+        public int? RealmId { get; set; }
+        public ICollection<WeaponDto> Weapons { get; set; }
+    }
+}
