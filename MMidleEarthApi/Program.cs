882bb947a967ff05ee46ab16e31616a1b1879bdd
gokcaneren <gokcann01@gmail.com>
2022-06-22 13:22:20 +0300

    Started to build dtos

--- MMidleEarthApi/Program.cs
+++ MMidleEarthApi/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
     x.UseSqlServer(builder.Configuration.GetConnectionString("db"), opt =>
---
+++ MiddleEarth.Core/DTOs/CharacterDto.cs
@@ -0,0 +1,18 @@
+﻿using MiddleEarth.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Core.DTOs
+{
+    public class CharacterDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public int RaceId { get; set; }
+        public int? RealmId { get; set; }
+        public ICollection<Weapon> Weapons { get; set; }
+    }
+}
