0454db94c4216411f889dd549c7321aa6e47a13b
gokcaneren <gokcann01@gmail.com>
2022-06-22 13:48:06 +0300

    Respons Dtos are added!

---
+++ MiddleEarth.Core/DTOs/CharacterFeatureDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Core.DTOs
+{
+    public class CharacterFeatureDto
+    {
+        public int Id { get; set; }
+        public string Sex { get; set; }
+        public int Height { get; set; }
+        public string Eyes { get; set; }
+        public string Hair { get; set; }
+        public int CharacterId { get; set; }
+    }
+}
---
+++ MiddleEarth.Core/DTOs/RaceDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Core.DTOs
+{
+    public class RaceDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
