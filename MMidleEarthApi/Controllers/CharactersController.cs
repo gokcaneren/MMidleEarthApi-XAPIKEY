5b61d98eb8c1d08a4a1b4b763fe1d8b757e00770
gokcaneren <gokcann01@gmail.com>
2022-06-24 12:58:58 +0300

    Http Methods are completed!

--- MMidleEarthApi/Controllers/CharactersController.cs
+++ MMidleEarthApi/Controllers/CharactersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MiddleEarth.Core.DTOs;
+using MiddleEarth.Core.DTOs.Requests;
 using MiddleEarth.Core.DTOs.Respons;
 using MiddleEarth.Core.Models;
 using MiddleEarth.Core.Services;
@@ -27,6 +28,37 @@ namespace MMidleEarthApi.Controllers
             var characters=await _service.GetAllAsync();
             var charactersDtos=_mapper.Map<List<CharacterDto>>(characters.ToList());
             return CreateActionResult(CustomResponsDto<List<CharacterDto>>.Success(200, charactersDtos));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var character = await _service.GetByIdAsync(id);
+            var characterDto= _mapper.Map<CharacterDto>(character);
+            return CreateActionResult(CustomResponsDto<CharacterDto>.Success(200, characterDto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(CharacterDto characterDto)
+        {
+            var character = await _service.AddAsync(_mapper.Map<Character>(characterDto));
+            var charactersDto = _mapper.Map<CharacterDto>(character);
+            return CreateActionResult(CustomResponsDto<CharacterDto>.Success(201, charactersDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CharacterUpdateDto characterDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Character>(characterDto));
+            return CreateActionResult(CustomResponsDto<NoContentDto>.Success(204));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var character = await _service.GetByIdAsync(id);
+            await _service.RemoveAsync(character);
+            return CreateActionResult(CustomResponsDto<NoContentDto>.Success(204));
         }
     }
 }
