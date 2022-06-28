8909a306828ab13ff2c0b6c8bd89c7e1e55e61a3
gokcaneren <gokcann01@gmail.com>
2022-06-28 13:29:31 +0300

    Generic Repo and Service added for CharacterWithRace

---
+++ MiddleEarth.Core/Services/ICharacterService.cs
@@ -0,0 +1,15 @@
+﻿using MiddleEarth.Core.DTOs.Respons;
+using MiddleEarth.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Core.Services
+{
+    public interface ICharacterService:IService<Character>
+    {
+        Task<CustomResponsDto<List<CharacterWithRaceDto>>> GetCharacterWithRace();
+    }
+}
