649b4b048e53e0145ed8c405280146d28a3e2566
gokcaneren <gokcann01@gmail.com>
2022-06-29 11:41:58 +0300

    Repos completed for RacesController

---
+++ MiddleEarth.Repository/Repositories/RaceRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiddleEarth.Core.Models;
+using MiddleEarth.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Repository.Repositories
+{
+    public class RaceRepository : GenericRepository<Race>, IRaceRepository
+    {
+        public RaceRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Race> GetSingleRaceByIdWithCharactersAsync(int raceId)
+        {
+            return await _context.Races.Include(x => x.Characters).Where(x => x.Id == raceId).SingleOrDefaultAsync();
+        }
+    }
+}
