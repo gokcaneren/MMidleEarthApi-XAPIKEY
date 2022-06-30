118acdd9fb0e11abdaab043bdbcb19222c8d6be9
gokcaneren <gokcann01@gmail.com>
2022-06-30 16:54:11 +0300

    Started to build Validations

---
+++ MiddleEarth.Service/Validations/CharacterDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MiddleEarth.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Service.Validations
+{
+    public class CharacterDtoValidator:AbstractValidator<CharacterDto>
+    {
+        public CharacterDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required!").NotEmpty().WithMessage(
+                "{PropertyName} is required!");
+        }
+    }
+}
