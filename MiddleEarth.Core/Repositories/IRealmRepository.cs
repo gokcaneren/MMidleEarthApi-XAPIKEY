14e9b4cea162d8103e09449c370886b705cbecc5
gokcaneren <gokcann01@gmail.com>
2022-06-30 13:54:06 +0300

    Started to build RealmsController

---
+++ MiddleEarth.Core/Repositories/IRealmRepository.cs
@@ -0,0 +1,14 @@
+﻿using MiddleEarth.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Core.Repositories
+{
+    public interface IRealmRepository:IGenericRepository<Realm>
+    {
+        Task<Realm> GetSingleRealmByIdWithCharactersAsync(int realmId);
+    }
+}
---
+++ MiddleEarth.Repository/Repositories/RealmRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiddleEarth.Core.Models;
+using MiddleEarth.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Repository.Repositories
+{
+    public class RealmRepository : GenericRepository<Realm>, IRealmRepository
+    {
+        public RealmRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Realm> GetSingleRealmByIdWithCharactersAsync(int realmId)
+        {
+            return await _context.Realms.Include(x=>x.Characters).Where(x=>x.Id==realmId).SingleOrDefaultAsync();
+        }
+    }
+}
