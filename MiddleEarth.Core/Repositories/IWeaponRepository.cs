4a1ad4882f0a2515383a9eca9ae444de6d7bbcda
gokcaneren <gokcann01@gmail.com>
2022-06-30 15:56:50 +0300

    Weapon Generic repo and normal repo added!

---
+++ MiddleEarth.Core/Repositories/IWeaponRepository.cs
@@ -0,0 +1,14 @@
+﻿using MiddleEarth.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Core.Repositories
+{
+    public interface IWeaponRepository:IGenericRepository<Weapon>
+    {
+        Task<Weapon> GetWeaponByIdWithCharactersAsync(int weaponId);
+    }
+}
---
+++ MiddleEarth.Repository/Repositories/WeaponRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiddleEarth.Core.Models;
+using MiddleEarth.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleEarth.Repository.Repositories
+{
+    public class WeaponRepository : GenericRepository<Weapon>, IWeaponRepository
+    {
+        public WeaponRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Weapon> GetWeaponByIdWithCharactersAsync(int weaponId)
+        {
+            return await _context.Weapons.Include(x=>x.Characters).Where(x=>x.Id==weaponId).SingleOrDefaultAsync();
+        }
+    }
+}
